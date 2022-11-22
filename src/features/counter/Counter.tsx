24421f94658530934f675ad43c11745f372769a8
thanh hai <hai.thanh.dinh@mgm-tp.com>
2022-11-22 19:03:13 +0700

    video 6 stop: 7:54

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -8,6 +8,7 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
+  incrementSaga,
 } from "./counterSlice";
 import styles from "./Counter.module.css";
 
@@ -50,11 +51,17 @@ export function Counter() {
         >
           Add Amount
         </button>
+        {/*<button*/}
+        {/*  className={styles.asyncButton}*/}
+        {/*  onClick={() => dispatch(incrementAsync(incrementValue))}*/}
+        {/*>*/}
+        {/*  Add Async*/}
+        {/*</button>*/}
         <button
           className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
+          onClick={() => dispatch(incrementSaga(incrementValue))}
         >
-          Add Async
+          Add Async Saga
         </button>
         <button className={styles.button} onClick={() => dispatch(incrementIfOdd(incrementValue))}>
           Add If Odd
--- src/features/counter/counterSaga.tsx
+++ src/features/counter/counterSaga.tsx
@@ -1,11 +1,14 @@
 import { takeEvery } from "redux-saga/effects";
 import { PayloadAction } from "@reduxjs/toolkit";
+import { incrementSaga } from "./counterSlice";
 
-export function* log(action: PayloadAction) {
-  console.log("log", action);
+// export function* log(action: PayloadAction) {
+//   console.log("log", action);
+// }
+function* handleIncrementSaga(payload: PayloadAction<number>) {
+  console.log("Handle Increment Saga: ", payload);
 }
-
 export default function* counterSaga() {
   console.log("counter Saga");
-  yield takeEvery("counter/increment", log);
+  yield takeEvery(incrementSaga.toString(), handleIncrementSaga);
 }
