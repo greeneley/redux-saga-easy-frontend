a47fc7ca89388ebded910161e70e40416fd28ce5
thanh hai <hai.thanh.dinh@mgm-tp.com>
2022-11-23 06:59:22 +0700

    end video 6: learn the difference between takeEvery and takeLatest

--- src/features/counter/counterSaga.tsx
+++ src/features/counter/counterSaga.tsx
@@ -1,14 +1,21 @@
-import { takeEvery } from "redux-saga/effects";
+import { delay, put, takeEvery, takeLatest } from "redux-saga/effects";
 import { PayloadAction } from "@reduxjs/toolkit";
-import { incrementSaga } from "./counterSlice";
+import { incrementSaga, incrementSagaSuccess } from "./counterSlice";
 
 // export function* log(action: PayloadAction) {
 //   console.log("log", action);
 // }
-function* handleIncrementSaga(payload: PayloadAction<number>) {
-  console.log("Handle Increment Saga: ", payload);
+function* handleIncrementSaga(action: PayloadAction<number>) {
+  console.log("Handle Increment Saga");
+
+  // wait 2s
+  yield delay(2000);
+  console.log("waiting done ");
+  // dispatch action success
+  yield put(incrementSagaSuccess(action.payload));
 }
 export default function* counterSaga() {
   console.log("counter Saga");
   yield takeEvery(incrementSaga.toString(), handleIncrementSaga);
+  // yield takeLatest(incrementSaga.toString(), handleIncrementSaga);
 }
